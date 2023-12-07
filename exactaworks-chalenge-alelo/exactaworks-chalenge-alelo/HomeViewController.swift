ce2df2f82a6ad053605dc3cdb866e21a83cc596f
Ygor Nascimento <ygor.nascimento@outlook.com>
2023-12-07 09:24:50 -0300

    Removing Main.storyboard and setup view code

---
+++ exactaworks-chalenge-alelo/exactaworks-chalenge-alelo/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  exactaworks-chalenge-alelo
+//
+//  Created by Ygor Nascimento on 07/12/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
